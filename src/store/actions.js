ddd308bfdb69b1dda08dd592127f22e10740776b
yellow yellow cat <30364534+susuang@users.noreply.github.com>
2018-05-15 15:55:24 +0800

    modify

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,7 @@
 import * as types from './mutation-type.js';
 
 export default {
-    setUserInfo({commit}, {login, name}) {
+    updateUserInfo({commit}, {login, name}) {
         commit(types.SET_USERNAME, name);
         commit(types.SET_LOGIN, login);
     }
